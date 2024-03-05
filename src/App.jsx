c6ab2d11d72280617c437586b0760248be31c54b
sleek-ennju <ihenachochukwudi@gmail.com>
2024-03-05 05:05:01 +0100

    Included map and added background colors to nput fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useState } from 'react'
+import React, { Suspense, useState } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './layout/Layout'
 import PreLoader from './pages/PreLoader'
