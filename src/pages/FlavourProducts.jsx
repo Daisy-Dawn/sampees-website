aaee573d9a5fe5e2c7684024ce1805b37f858645
Daisy-Dawn <agboella20@gmail.com>
2024-03-09 18:00:09 +0100

    updates made

--- src/pages/FlavourProducts.jsx
+++ src/pages/FlavourProducts.jsx
@@ -15,7 +15,7 @@ const FlavourProducts = () => {
       description:
         'Indulge in the rich and comforting taste of our De-Sunnex Mix Milk Flavor Powder. Made with a blend of premium ingredients including powdered milk, granulated sugar, and milk flour, this flavor powder is a versatile addition to your culinary creations. Our FlavourPowder captures the essence of creamy, delicious milk in a convenient powder form.',
       ingredients:
-        'Corn Starch, Powdered Milk, Milk Flavour, Butyric Acid, Vanillin',
+        'Corn Starch, Powdered Milk, Milk Flavour, Butyric Acid',
       grams: {
         big: '500g',
         small: '100g'
@@ -29,9 +29,9 @@ const FlavourProducts = () => {
     {
       header: 'PEE-SABINA VANILLA FLAVOUR POWDER',
       description:
-        'Indulge in the rich and comforting taste of our De-Sunnex Mix Milk Flavor Powder. Made with a blend of premium ingredients including powdered milk, granulated sugar, and milk flour, this flavor powder is a versatile addition to your culinary creations. Our FlavourPowder captures the essence of creamy, delicious milk in a convenient powder form.',
+        'Indulge in the exquisite aroma and delicate sweetness of our Pee-Sabina Vanilla Flavour Powder. Crafted with care using the finest ingredients, this powder adds a touch of sophistication to any dish. Savor the smooth, velvety taste of our Vanilla Flavor Powder, perfect for enhancing desserts, beverages, and baked goods alike. Elevate your culinary creations with the timeless allure of vanilla, creating unforgettable flavors that delight the senses. ',
       ingredients:
-        'Corn Starch, Powdered Milk, Vanilla Flavour, Butyric Acid, Vanillin',
+        'Corn Starch, Powdered Milk, Vanilla Flavour, Butyric Acid',
       grams: {
         small: '100g'
       },
@@ -43,9 +43,9 @@ const FlavourProducts = () => {
     {
       header: 'DE-SUNNEX MIX BANANA FLAVOUR POWDER',
       description:
-        "Immerse yourself in the tropical delight of our De-Sunnex Mix Banana Flavor Powder. Made with premium ingredients, including ripe banana essence, sugar, and natural flavorings, this powder encapsulates the luscious sweetness and distinctive aroma of freshly-picked bananas. Whether you're creating banana-flavored cakes, shakes, or ice creams, this powder promises to infuse your creations with irresistible banana goodness.",
+        "Immerse yourself in the tropical delight of our De-Sunnex Mix Banana Flavor Powder. Made with premium ingredients, including ripe banana essence, sugar, and natural flavorings, this powder encapsulates the luscious sweetness and distinctive aroma of freshly-picked bananas. This flavour is the most favorite for making custard powders. Whether you're creating banana-flavored cakes, shakes, custard or ice creams, this powder promises to infuse your creations with irresistible banana goodness.",
       ingredients:
-        'Corn Starch, Powdered Milk, Banana Flavour, Butyric Acid, Vanillin',
+        'Corn Starch, Powdered Milk, Banana Flavour, Butyric Acid',
       grams: {
         small: '80g'
       },
@@ -57,9 +57,9 @@ const FlavourProducts = () => {
     {
       header: 'DE-SUNNEX MIX COCONUT FLAVOUR POWDER',
       description:
-        "Embark on a journey to the tropics with our De-Sunnex Mix Coconut Flavor Powder. Crafted with the finest ingredients, including coconut extract, sugar, and natural flavorings, this powder captures the essence of paradise in every spoonful. Whether you're adding it to smoothies, desserts, or baked goods, its rich coconut flavor will evoke memories of tropical bliss.{' '}",
+        "Embark on a journey to the tropics with our De-Sunnex Mix Coconut Flavor Powder. Crafted with the finest ingredients, including coconut extract, sugar, and natural flavorings, this powder captures the essence of paradise in every spoonful. Whether you're adding it to smoothies, desserts, or baked goods, its rich coconut flavor will evoke memories of tropical bliss.",
       ingredients:
-        'Corn Starch, Powdered Milk, Coconut Flavour, Butyric Acid, Vanillin',
+        'Corn Starch, Powdered Milk, Coconut Flavour, Butyric Acid',
       grams: {
         small: '80g'
       },
@@ -73,7 +73,7 @@ const FlavourProducts = () => {
       description:
         'Indulge in the tropical delight of our Pineapple Flavour Powder, a delicious addition to your culinary adventures. Made from the finest ingredients, our Pineapple Flavour Powder is crafted to perfection to elevate your recipes to new heights.  Whether sprinkled over desserts, blended into smoothies, or used in baking, this powder adds a delightful twist to any recipe.',
       ingredients:
-        'Corn Starch, Powdered Milk, Pineapple Flavour, Butyric Acid, Vanillin',
+        'Corn Starch, Powdered Milk, Pineapple Flavour, Butyric Acid',
       grams: {
         small: '80g'
       },
